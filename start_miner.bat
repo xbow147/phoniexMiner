6e3e8d9c8b9d88537985577d7eaa88127da50e2d
xbow147 <62348274+xbow147@users.noreply.github.com>
2020-12-31 14:46:32 +0200

    Update start_miner.bat

--- start_miner.bat
+++ start_miner.bat
@@ -1,14 +1,8 @@
-REM
-REM Example bat file for starting PhoenixMiner.exe to mine ETH
-REM
-
 setx GPU_FORCE_64BIT_PTR 0
 setx GPU_MAX_HEAP_SIZE 100
 setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-PhoenixMiner.exe -pool ssl://eu1.ethermine.org:5555 -wal 0x39e987c72d3b35da03ba9aca26a9e9f91c16c04c.Rig001 -dagrestart 1 -rvram -1 -eres 0
+PhoenixMiner.exe -pool eu1.ethermine.org:4444 -ewal 0x198e49823d7d9d6ab34b30fa10ee54ffeecc9e16.rig_1 -epsw x -amd -acm -mi 1 -gt 126 -mode 1 -clKernel 1 -clNew 1 -clf 0 -lidag 1 -openclLocalWork 64 -openclGlobalMultiplier 4096
 pause
-
